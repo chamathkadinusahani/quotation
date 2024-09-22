89d10ceb8473c3bd54b2f1dc34789fd870044584
chamathkadinusahani <chamathkadinusahani1@gmail.com>
2024-09-22 22:47:22 +0530

    Update MyComponent.jsx

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -25,22 +25,23 @@ export default function MyComponent() {
                   srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/fb0f4408458057004673e69632b769a14759e57120a7e7f5f72d67e709448500?placeholderIfAbsent=true&apiKey=170b590e187e4beabe3f2ef7100234b5&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/fb0f4408458057004673e69632b769a14759e57120a7e7f5f72d67e709448500?placeholderIfAbsent=true&apiKey=170b590e187e4beabe3f2ef7100234b5&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/fb0f4408458057004673e69632b769a14759e57120a7e7f5f72d67e709448500?placeholderIfAbsent=true&apiKey=170b590e187e4beabe3f2ef7100234b5&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/fb0f4408458057004673e69632b769a14759e57120a7e7f5f72d67e709448500?placeholderIfAbsent=true&apiKey=170b590e187e4beabe3f2ef7100234b5&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/fb0f4408458057004673e69632b769a14759e57120a7e7f5f72d67e709448500?placeholderIfAbsent=true&apiKey=170b590e187e4beabe3f2ef7100234b5&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/fb0f4408458057004673e69632b769a14759e57120a7e7f5f72d67e709448500?placeholderIfAbsent=true&apiKey=170b590e187e4beabe3f2ef7100234b5&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/fb0f4408458057004673e69632b769a14759e57120a7e7f5f72d67e709448500?placeholderIfAbsent=true&apiKey=170b590e187e4beabe3f2ef7100234b5&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/fb0f4408458057004673e69632b769a14759e57120a7e7f5f72d67e709448500?placeholderIfAbsent=true&apiKey=170b590e187e4beabe3f2ef7100234b5"
                   className={styles.img2}
                 />
-              </div>
+              </div >
               <div className={styles.column3}>
                 <div className={styles.div5}>
                   <div className={styles.sendQuotation}>SEND QUOTATION</div>
-                  <div className={styles.inputFieldWithLabel}>
+                 <div className={styles.inputFieldWithLabel}>
                     <div className={styles.label}>Email</div>
-                    <div className={styles.inputField}>
+                    
+                    < input type="email" className={styles.inputField}/>
                       <div className={styles.wrapper} />
-                    </div>
+                    
                   </div>
                   <div className={styles.inputFieldWithLabel2}>
                     <div className={styles.label2}>Description</div>
                   </div>
-                  <div className={styles.inputField2}>
+                  < input type="text" className={styles.inputField2}/>
                     <div className={styles.wrapper2} />
-                  </div>
+                 
                 </div>
               </div>
             </div>
